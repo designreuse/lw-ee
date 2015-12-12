23c7e040c2e0bdc73f8885d82bd69e65d31c0c74
Nikolay Kuzmenkov <n.kuzm@rambler.ru>
2015-12-12 13:56:49 +0400

    Controllers and changes in web.xml added.

---
+++ src/main/java/ru/tsystems/javaschool/kuzmenkov/logiweb/controllers/DriverController.java
@@ -0,0 +1,10 @@
+package ru.tsystems.javaschool.kuzmenkov.logiweb.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Nikolay Kuzmenkov.
+ */
+@Controller
+public class DriverController {
+}
